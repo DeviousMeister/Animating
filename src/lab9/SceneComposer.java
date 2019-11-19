41cd44c28ef9e9a826753cb814d4c02848d124ba
Moises <mdaboin@wustl.edu>
2019-11-19 11:33:23 -0600

    Thank you, this lab wasn't as hard as the past ones

--- src/lab9/SceneComposer.java
+++ src/lab9/SceneComposer.java
@@ -14,36 +14,65 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class SceneComposer {
 
-
-
 	public static void main(String[] args) {
 
 		ArgsProcessor ap = new ArgsProcessor(args);
-		//
-		// for demo only, remove this code and write your own to do what
-		//   is needed for this lab
-		while(true) {
-			StdDraw.show(10);
-			for (int i=0; i < 10; ++i) {
-				Forest f = new Forest(5);
-				f.draw(); f.draw(); f.draw(); f.draw();
-				Leaves l = new Leaves(5);
-				l.draw(); l.draw();
+		Map<String, Drawable> init = new HashMap<>();
+		List<Drawable> firstDraw = new LinkedList<Drawable>();
+		List<Drawable> newDraws = new LinkedList<Drawable>();
+
+		firstDraw.add(new Forest(10));
+		firstDraw.add(new Leaves(5));
+		firstDraw.add(new Bubbles(15));
+		firstDraw.add(new Dragon(0.1, 0.5, 0.3));
+
+		Sequence seqInit = new Sequence(firstDraw);
+		init.put("init", seqInit);
+
+		while (true) {
+			StdDraw.show(100);
+			String resp = ap.nextString("Express yourself: ");
+			if(init.get(resp)!=null){
+				init.get(resp).draw();
 			}
-			Bubbles b = new Bubbles(10);
-			b.draw();
-			StdDraw.show(10);
-			String resp = ap.nextString("Again?");
-			if (resp.equals("no")) {
+			if(resp.equals("record")){
+				boolean recordOn = true;
+				while(recordOn){
+					String whatAdd = ap.nextString("What do you want to add?");
+
+						if(whatAdd.equals("bubbles")){
+							newDraws.add(new Bubbles(10));
+						}
+						if(whatAdd.equals("forests")){
+							newDraws.add(new Forest(20));
+						}
+						if(whatAdd.equals("circles")){
+							newDraws.add(new Circle(6, 5, 0.2));
+						}
+						if(whatAdd.equals("polygons")){
+							newDraws.add(new Poly(9));
+						}
+						if(whatAdd.equals("leaves")){
+							newDraws.add(new Leaves(8));
+						}
+
+					else if(whatAdd.equals("done")){
+						String name = ap.nextString("What do you want to name this?");
+						Sequence newSeq = new Sequence(newDraws);
+						init.put(name, newSeq);
+						init.get(name).draw();
+						recordOn = false;
+					}
+				}
+			}
+			if (resp.equals("end")) {
 				break;
 			}
-			else {
+			if (resp.equals("clear")) {
 				new Clear().draw();
 			}
+			StdDraw.show(100);
 		}
-		//
-		// end of demo code
-		//
 	}
 
 }
